03fa6bf3bf265560d3bf0fb447a23efd9084ed95
Vlad Ageysha <ander123fakk@gmail.com>
2022-10-20 15:55:09 +0300

    Set styles for login and register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,58 @@
 @extends('layouts.base')
 
+@section('styles')
+    <link rel="stylesheet" href="/styles/form.css">
+@endsection
+@section('title', 'Вход')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="container">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+        <form class="form" method="POST" action="{{ route('register') }}">
+            @csrf
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+            <h2 class="form__header">Регистрация</h2>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <div class="form__container">
+                <input id="name" type="text" class="form__input @error('name') is-invalid @enderror" name="name"
+                    value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Имя">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
             </div>
-        </div>
+
+            <div class="form__container">
+                <input id="email" type="email" class="form__input @error('email') is-invalid @enderror" name="email"
+                    value="{{ old('email') }}" required autocomplete="email" placeholder="Электронная почта">
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+
+            <div class="form__container">
+                <input id="password" type="password" class="form__input @error('password') is-invalid @enderror"
+                    name="password" required autocomplete="new-password" placeholder="Пароль">
+
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+            <div class="form__container">
+                <input id="password-confirm" type="password" class="form__input" name="password_confirmation" required
+                    autocomplete="new-password" placeholder="Повторите пароль">
+            </div>
+
+            <button type="submit" class="form__submit">Регистрация</button>
+        </form>
     </div>
-</div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,15 +27,33 @@
         <form class="form form-add_hide" id="form-add">
             @csrf
             <h2 class="form__header">Добавить компанию</h2>
+
             <div class="form__container">
                 <input class="form__input" type="text" name="name" placeholder="Название" required>
+            </div>
+
+            <div class="form__container">
                 <input class="form__input" type="number" name="inn" placeholder="ИНН" required>
+            </div>
+
+            <div class="form__container">
                 <textarea class="form__textarea" name="info" placeholder="Информация о компании" required></textarea>
+            </div>
+
+            <div class="form__container">
                 <input class="form__input" type="text" name="gen_director" placeholder="Генеральный директор" required>
+            </div>
+
+            <div class="form__container">
                 <input class="form__input" type="text" name="address" placeholder="Адрес" required>
+            </div>
+
+            <div class="form__container">
                 <input class="form__input" type="tel" name="tel" placeholder="Телефон" required>
             </div>
+
             <input class="form__submit" type="submit" value="Добавить">
+            
         </form>
     </section>
 
